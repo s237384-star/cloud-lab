c695a2f379e1cd12f1f84fd0ce13fe38c1525de7
Nguyen Tran Doan Trang <s237384@ntcu.edu.vn>
2026-09-09 15:27:05 +0700

    Add request logging

--- mern-demo/server/server.js
+++ mern-demo/server/server.js
@@ -11,6 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Log các request từ Frontend
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 const PORT = process.env.PORT || 5000;
 
 // Kết nối MongoDB Atlas
